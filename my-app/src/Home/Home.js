15b0aef761ff0b715132a49140debbc7ef1f20d4
Bozo <b4080024@gmail.com>
2024-04-12 13:06:45 +0200

    afsdasdf

--- my-app/src/Home/Home.js
+++ my-app/src/Home/Home.js
@@ -1,11 +1,52 @@
-
+import { useState } from "react";
 
 function Home(props){
-console.log(props.pod);
-    const tekst = "Ovo je moja prva aplikacija";
-    return <div>
+    const [tekst, setTekst] = 
+    useState("Ovo je moja prva aplikacija");
+
+    const[ime, setIme] = useState("Bozo");
+    const[godine, setGodine] = useState("16");
+    const[novoIme, setNovoIme] = useState("");
+    const[noveGodine, setNoveGodine] = useState("");
+
+
+    const handleOnClick = (event) => {
+        event.preventDefault();
+        setIme(novoIme);
+        setGodine(noveGodine);
+        setNovoIme("");
+        setNoveGodine("");
+        props.funkcija(futerData)
+      };
+
+return <>
     <h1>{props.naslov}</h1>
-    <div>{tekst}</div></div>
-}
+    <div>{tekst}</div>
+    
+<form onSubmit={handleOnClick}>
+    <label>
+        ine:
+        <input
+        type="text"
+        value={Novo ime}
+        onChange={(event) => setNovoIme(event.target.value)}
+    </label>
+<br/>
+<label>
+    Godine
+    <input
+    type="number"
+    value={NoveGodine}
+    onChange={(event) =>{
+        setNoveGodine(event.target.value);
+        console.log(event);
+    }}
+/>
+</label>
+<button type = "submit">SUBMIT</button>
+</form>
+<div/>
+
+</>
 
 export default Home;
